6a05b1410748c0e577bc068acf57a1670cc7d544
Joshua Boggs <joshua.j.boggs@gmail.com>
2014-07-28 01:16:26 -0700

    solved problem 3: end_x

--- recursion.py
+++ recursion.py
@@ -8,6 +8,7 @@ def all_star(input_string):
 
 print("all_star(\"joshua\") --> " + all_star("joshua") + "\n")
 
+
 def pair_star(input_string):
   if len(input_string) == 0:
     return ""
@@ -19,3 +20,20 @@ def pair_star(input_string):
     return input_string[0] + pair_star(input_string[1:])
 
 print("pair_star(\"jjoosshua\") --> " + pair_star("jjoosshua") + "\n")
+
+
+def end_x(input_string):
+  if len(input_string) == 0:
+    return ""
+  elif input_string[0] == "x":
+    return end_x(input_string[1:]) + "x"
+  else:
+    return input_string[0] + end_x(input_string[1:])
+
+print("end_x(\"jxxoshxuxax\") --> " + end_x("jxxoshxuxax"))
+
+
+
+
+
+print("\n")
